cc1bfc1d72dda9591d10424ebe4a4cadeb514ca7
Evgeniy Pelikh <pelikh123.evg@gmail.com>
2023-07-19 12:26:58 +0200

    added shop template, created models for shop app, configured rout to shop app

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField(null=True)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField(null=True)
+    created_at = models.DateTimeField(default=timezone.now)
+    quantity = models.IntegerField(default=0)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return f"Name: {self.name}, Category: {self.category}"
