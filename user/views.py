00cbcde41b0d677ac4e66cf02a0e8ed3454c9549
Evgeniy Pelikh <pelikh123.evg@gmail.com>
2023-08-04 09:06:00 +0200

    made form for user login

--- user/views.py
+++ user/views.py
@@ -1,19 +1,21 @@
 from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
+from . forms import UserLoginForm
 
 
-def registration(request):
+def registration(request: HttpRequest) -> HttpResponse:
     context = {}
 
     return render(request, 'user/registration.html', context)
 
 
-def login(request):
-    context = {}
+def login(request: HttpRequest) -> HttpResponse:
+    context = {'form': UserLoginForm()}
 
     return render(request, 'user/login.html', context)
 
 
-def profile_cart(request):
+def profile_cart(request: HttpRequest) -> HttpResponse:
     context = {}
 
     return render(request, 'user/profile_cart.html', context)
