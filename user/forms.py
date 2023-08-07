de2ebb54d42d8f21748506e2121864b147c79cb7
Evgeniy Pelikh <pelikh123.evg@gmail.com>
2023-08-07 09:18:30 +0200

    UserLoginForm have been created and connected

--- user/forms.py
+++ user/forms.py
@@ -1,9 +1,19 @@
 from django.contrib.auth.forms import AuthenticationForm
+from django import forms
 from . models import User
 
 
 class UserLoginForm(AuthenticationForm):
+    username = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'your_name',
+        'placeholder': 'Your Name'
+    }))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'class': 'your_pass',
+        'placeholder': 'Password'
+
+    }))
+
     class Meta:
         model = User
         fields = ['username', 'password']
-    
