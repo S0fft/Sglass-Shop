b140b278a1747c49275d9f1c6945027e235abe7a
Evgeniy Pelikh <pelikh123.evg@gmail.com>
2023-08-09 12:20:13 +0200

    created models and methods for basket

--- shop/models.py
+++ shop/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from user.models import User
 
 
 class Category(models.Model):
@@ -21,7 +22,8 @@ class Product(models.Model):
     created_at = models.DateTimeField(default=timezone.now)
     quantity = models.IntegerField(default=0)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    image = models.ImageField(null=True, blank=True, upload_to='products_images')
+    image = models.ImageField(null=True, blank=True,
+                              upload_to='products_images')
 
     def __str__(self) -> str:
         return f"Name: {self.name}, Category: {self.category}"
@@ -29,3 +31,26 @@ class Product(models.Model):
     class Meta:
         verbose_name_plural = "Products"
         verbose_name = 'Product'
+
+
+class BasketQuerySet(models.QuerySet):
+    def total_sum(self) -> int:
+        return sum(basket.sum() for basket in self)
+
+    def total_quantity(self) -> int:
+        return sum(basket.quantity for basket in self)
+
+
+class Basket(models.Model):
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    product = models.ForeignKey(to=Product, on_delete=models.CASCADE)
+    quantity = models.PositiveSmallIntegerField(default=0)
+    created_timestamp = models.DateTimeField(auto_now_add=True)
+
+    objects = BasketQuerySet.as_manager()
+
+    def __str__(self) -> str:
+        return f'Cart for: {self.user.username} | Product: {self.product.name}'
+
+    def mult_product(self) -> int:
+        return self.product.price * self.quantity
