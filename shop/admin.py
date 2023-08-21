645e490cb7d8310cf12058b5e6493ac83f1cadef
Evgeniy Pelikh <pelikh123.evg@gmail.com>
2023-08-21 10:36:18 +0200

    added new fields and settings to user admin

--- shop/admin.py
+++ shop/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from . models import Category, Product
+from . models import Category, Product, Basket
 
 admin.site.register(Category)
 
@@ -10,3 +10,10 @@ class ProductAdmin(admin.ModelAdmin):
     fields = ['name', 'category', 'description', 'price', 'quantity', 'image']
     readonly_fields = ['price']
     search_fields = ['name']
+
+
+class BasketAdmin(admin.TabularInline):
+    model = Basket
+    fields = ['product', 'quantity', 'created_timestamp']
+    readonly_fields = ['created_timestamp']
+    extra = 0
